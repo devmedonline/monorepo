4eac74591ea56d8ae5e3f87ba00b196b81d19de1
lucas rego <lucasalvesrego.contato@gmail.com>
2023-12-27 18:41:21 -0300

    feat: ui components

--- apps/web/src/shared/lib/lib.ts
+++
@@ -1,8 +0,0 @@
-import { type ClassValue as ClsxClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export type ClassValue = ClsxClassValue;
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
